fad895cce56596620f03867b2a6ed88168737d51
Hamza <132839490+Hamza-180@users.noreply.github.com>
2024-05-06 19:15:22 +0200

    improve

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,23 +3,29 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        // Create a ScheduledExecutorService with a single thread
+        ScheduledExecutorService heartbeatScheduler = Executors.newScheduledThreadPool(1);
 
+        // Schedule the heartbeat task to run every second
+        heartbeatScheduler.scheduleAtFixedRate(Main::sendHeartbeat, 0, 1, TimeUnit.SECONDS);
+    }
 
-        // we create a ScheduledExecutorService to schedule the heartbeats to be sent within an interval
-        ScheduledExecutorService heartbeatScheduler = Executors.newScheduledThreadPool(1); // use of one thread to execute the scheduled heartbeats
-
-        // we schedule the task to send heartbeats every second
-        heartbeatScheduler.scheduleAtFixedRate(() -> {
-            try {
-                Heartbeat heartbeat = new Heartbeat();
-                heartbeat.sendHeartbeat();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }, 0, 1, TimeUnit.SECONDS);
-
+    // Static method to send heartbeats
+    private static void sendHeartbeat() {
+        Heartbeat heartbeat = new Heartbeat();
+        try {
+            heartbeat.sendHeartbeat();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
-
+// Heartbeat class
+class Heartbeat {
+    // Send heartbeat method
+    void sendHeartbeat() throws Exception {
+        // Implementation goes here
+    }
+}
